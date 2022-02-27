eadf7b9105bd80b149625834bd38dab316905c3b
overinvested <67354344+overinvested@users.noreply.github.com>
2022-02-27 16:04:06 -0500

    added parsing and output

--- stock_chatter.py
+++ stock_chatter.py
@@ -3,6 +3,7 @@
 from sys import argv
 import nltk
 import classify
+from parse import parse
 
 def main():
     try:
@@ -13,16 +14,26 @@ def main():
 def runFromFile(article1, article2, filename):
     file = open(filename, 'r')
     for line in file:
-        print(line.strip())
-        # replace with parser
+        result = parse(article1, article2, line.strip())
+        if result:
+            print(result[0])
+            for sentence in result[1]:
+                print('\nArticle:%d Line %d: %s', sentence[2], sentence[1], sentence[0])
+        else:
+            print('No information available.')
     file.close()
 
 def runFromConsole(article1, article2):
     userInput = ""
     while userInput != 'quit':
         userInput = input('What would you like to know?\n')
-        print(userInput, article1, article2)
-        # replace with parser
+        result = parse(article1, article2, userInput)
+        if result:
+            print(result[0])
+            for sentence in result[1]:
+                print('\nArticle:%d Line %d: %s', sentence[2], sentence[1], sentence[0])
+        else:
+            print('No information available.')
 
 
 if __name__ == "__main__":
