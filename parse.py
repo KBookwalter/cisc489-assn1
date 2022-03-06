023fb164678367cccc70443f316218dc8313fb3d
Kevin Bookwalter <kbookwal@udel.edu>
2022-03-05 22:49:56 -0500

    sweet, sweet victory

--- parse.py
+++ parse.py
@@ -12,7 +12,12 @@ import re
 rise_words = get_syns(["go_up", "rise", "increase", "jump", "ascend", "went_up", "rose", "increased", "jumped", "ascended", "up"])
 fall_words = get_syns(["fall", "drop", "dip", "decrease", "go_down", "fell", "dropped", "dipped", "decreased", "down", "loss", "dive", "lower", "off"])
 open_words = get_syns(["open", "start", "begin", "opened", "started", "began"])
-close_words = get_syns(["close", "end", "finish", "closed", "ended", "finished"])
+close_words = get_syns(["close", "end", "finish","closed_down", "closed", "ended", "finished"])
+
+# for word in close_words:
+#     print(word)
+
+# i = 1/0
 
 
 
@@ -31,14 +36,14 @@ def parse(article: str, question: str) -> list:
     if not sentences:
         return None
 
-    print('\n\nQUESTION INFO:\n')
-    print(questionInfo[0])
-    print(questionInfo[1])
-    print('\n')
-    for sentence in sentences:
-        print(sentence[0])
-        print('\n')
-    print('\n\n')
+    # print('\n\nQUESTION INFO:\n')
+    # print(questionInfo[0])
+    # print(questionInfo[1])
+    # print('\n')
+    # for sentence in sentences:
+    #     print(sentence[0])
+    #     print('\n')
+    # print('\n\n')
 
     result = []
 
@@ -63,25 +68,29 @@ def parse(article: str, question: str) -> list:
     elif(questionInfo[0] == QuestionType.AMT_RISE):
         result = parseQuant(sentences, rise_words, questionInfo[1])
         if result:
-            return ['%s.' % result[0][1], [list(result[0][0])]]
+            # return ['%s.' % result[0][1], [list(result[0][0])]]
+            return ['QUANT_FLAG', result]
         else:
             return None
     elif(questionInfo[0] == QuestionType.AMT_FALL):
         result = parseQuant(sentences, fall_words, questionInfo[1])
         if result:
-            return ['%s.' % result[0][1], [list(result[0][0])]]
+            # return ['%s.' % result[0][1], [list(result[0][0])]]
+            return ['QUANT_FLAG', result]
         else:
             return None
     elif(questionInfo[0] == QuestionType.AMT_OPEN):
         result = parseQuant(sentences, open_words, questionInfo[1])
         if result:
-            return ['%s.' % result[0][1], [list(result[0][0])]]
+            # return ['%s.' % result[0][1], [list(result[0][0])]]
+            return ['QUANT_FLAG', result]
         else:
             return None
     elif(questionInfo[0] == QuestionType.AMT_CLOSE):
         result = parseQuant(sentences, close_words, questionInfo[1])
         if result:
-            return ['%s.' % result[0][1], [list(result[0][0])]]
+            # return ['%s.' % result[0][1], [list(result[0][0])]]
+            return ['QUANT_FLAG', result]
         else:
             return None
     else:
@@ -148,8 +157,17 @@ def parseQuant(sentences: str, words: list, entityName: str) -> list:
     foundSentence = 0
     for sentence in sentences:
         for word in words:
-            # regex = entityName.lower() + r'\s(?:\D{1,50})?\s' + str(word).lower() + r'\s(?:\D{1,15})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]|' + entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower()
-            regex = entityName.lower() + r'(?:\s.{1,50})?\s' + str(word).lower() + r'\s(?:\D{1,15})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]|' + entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower()
+
+            regex=''
+            if words[0] == 'stopping point':
+                # print('\n\n\n\n')
+                # print('HERE')
+                # print('\n\n\n\n')
+                # regex = entityName.lower() + r'\s(?:\D{1,50})?\s' + str(word).lower() + r'\s(?:\D{1,15})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]|' + entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower()
+                # regex = entityName.lower() + r'(?:\s.{1,50})?\s' + str(word).lower() + r'(?:.+at)?\s(?:\D{1,10})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]|' + entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower()
+                regex =  entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower() + r'|'+entityName.lower() + r'(?:\s.{1,50})?\s' + str(word).lower() + r'(?:.{1,20}(?:at|to))?\s(?:\D{1,10})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]'
+            else:
+                regex =  entityName.lower() + r'\s(?:\D{1,125})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)(?:\D{1,15})?(?:\d+(?:\.\d+)?%)?(?:\D{1,15})?\s'+ str(word).lower() + r'|'+entityName.lower() + r'(?:\s.{1,50})?\s' + str(word).lower() + r'\s(?:\D{1,10})?(\d+(?:\.\d+)?%?(?:\s\d/\d)?)[\s.,-]'
             match = re.search(regex, sentence[0].lower())
             if match:
                 if match.group(1):
--- stock_chatter.py
+++ stock_chatter.py
@@ -8,19 +8,30 @@ from parse import parse
 def main():
     try:
         runFromFile(argv[1], argv[2])
-    except:
+    except IndexError:
         runFromConsole(argv[1])
 
+# TODO: handle flag
 def runFromFile(article, filename):
     file = open(filename, 'r')
     for line in file:
-        result = parse(article, line.strip())
+        q = line.strip()
+        result = parse(article, q)
+        print(q)
         if result:
-            print('Answer: %s\n' % result[0])
-            for sentence in result[1]:
-                print('Source: Line %d: %s\n' % (sentence[1], sentence[0]))
+            if result[0] == 'QUANT_FLAG':
+                # print('QUANT_FLAG')
+                for r in result[1]:
+                    print('Answer: %s' % r[1])
+                    print('Source: Line %s: %s\n' % (r[0][1], r[0][0]))
+
+            else:
+                print('\n')
+                print('Answer: %s\n' % result[0])
+                for sentence in result[1]:
+                    print('Source: Line %d: %s\n' % (sentence[1], sentence[0]))
         else:
-            print('No information available.')
+            print('No information available.\n')
     file.close()
 
 def runFromConsole(article):
@@ -31,15 +42,21 @@ def runFromConsole(article):
             break
         result = parse(article, userInput)
 
-        print('\n\n')
-        print(result)
-        print('\n\n')
+        # print('\n\n')
+        # print(result)
+        # print('\n\n')
 
         if result:
-            print('\n')
-            print('Answer: %s\n' % result[0])
-            for sentence in result[1]:
-                print('Source: Line %d: %s\n' % (sentence[1], sentence[0]))
+            if result[0] == 'QUANT_FLAG':
+                for r in result[1]:
+                    print('Answer: %s' % r[1])
+                    print('Source: Line %s: %s\n' % (r[0][1], r[0][0]))
+
+            else:
+                print('\n')
+                print('Answer: %s\n' % result[0])
+                for sentence in result[1]:
+                    print('Source: Line %d: %s\n' % (sentence[1], sentence[0]))
         else:
             print('No information available.\n')
 
